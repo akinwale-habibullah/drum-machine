2e38958d0dc3ec5ede0301bd9724b4f1ab6792b8
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-13 23:40:28 +0100

    make switch styles global

--- src/components/TypeSwitch.js
+++ src/components/TypeSwitch.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const PowerSwitch = (props) => {
+const TypeSwitch = (props) => {
   return (
     <div className="switch-container">
       <div className="switch">
         <div className="switch-button"></div>
       </div>
     </div>
-  );
+  )
 }
 
-export default PowerSwitch;
+export default TypeSwitch;
