35c04f1f022b002ed4a485f3362bf81bd3a2be85
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-13 23:08:01 +0100

    add powerswitch component

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Controls.css';
+import PowerSwitch from './PowerSwitch';
 
 const Controls = (props) => {
   return (
-    <div className="controls">
-      <div className="power-switch">1</div>
+    <div className="control-panel">
+      <PowerSwitch className="power-switch" />
       <div className="label">2</div>
       <div className="volume">3</div>
       <div className="type-switch">4</div>
---
+++ src/components/PowerSwitch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PowerSwitch.css';
+
+const PowerSwitch = (props) => {
+  return (
+    <div className="power-switch-container">
+      <div className="power-switch">
+        <div className="switch-button"></div>
+      </div>
+    </div>
+  );
+}
+
+export default PowerSwitch;
