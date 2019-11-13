d9d0993c5838e3f1b1f4ef9fdf9c52b81aa33962
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-13 23:40:48 +0100

    add label component

---
+++ src/components/Label.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Label.css';
+
+const Label = (props) => {
+  return (
+    <div className="label-block">
+      <div className="label"></div>
+    </div>
+  )
+}
+
+export default Label;
