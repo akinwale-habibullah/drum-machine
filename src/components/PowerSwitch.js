e28be2c137afcd4a1ec17ad1b9d866368a928c49
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-14 19:20:12 +0100

    add label to power and bank switches and also add toggle functionality to power switch component

--- src/components/PowerSwitch.js
+++ src/components/PowerSwitch.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
-const PowerSwitch = (props) => {
-  return (
-    <div className="switch-container">
-      <div className="switch">
-        <div className="switch-button"></div>
+class PowerSwitch extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      status: 'off'
+    }
+
+    this.handleSwitch = this.handleSwitch.bind(this);
+  }
+
+  handleSwitch(e){
+    this.setState((state, props) => ({
+      status: !state.status
+    }));
+  }
+
+  render(){
+    return (
+      <div className="switch-container" style={{paddingLeft: "20%"}}>
+        <div style={{alignItems: "center",display: "grid", gridTemplateColumns: "0.8fr 1.2fr", height: "100%", justifyItems: "right"}}>
+          <div>Power</div>
+          <div className="switch" onClick={this.handleSwitch}>
+            {this.state.status === true ? <div className="switch-button" style={{justifySelf: "right"}}></div> : <div className="switch-button"></div> }
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default PowerSwitch;
